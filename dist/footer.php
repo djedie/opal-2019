2b9cefa2b6954f46addd394d2e7094eba6be6ffe
Jérôme Dumont <jerome@omts.fr>
2019-04-07 21:41:48 +0200

    First upload

---
+++ dist/footer.php
@@ -0,0 +1,54 @@
+<div class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 d-md-none d-lg-block">
+        <h3 class="main-logo"><a href="/"><img src="img/logo-couleur.png" width="148" height="158" alt="Opal - 1 au 5 août 2019"></a></h3>
+      </div>
+      <div class="col-md-9 col-lg-6">
+        <ul class="list-unstyled links-list">
+          <li><a href="/vision.php">La vision d'Opal</a></li>
+          <li><a href="/photos.php">Les photos 2017-2018</a></li>
+          <li><a href="https://trello.com/c/kY8EKSYo/35-faq" target="_blank">FAQ</a></li>
+          <li><a href="#" target="_blank">Billetterie</a></li>
+<!--           <li><a href="http://opal-festival.com/wiki" target="_blank">Wiki</a></li> -->
+          <li><a href="#" class="email">Contacte-nous</a></li>
+          <li><a href="https://projects.invisionapp.com/boards/ZR3LGQUB3W2#/" target="_blank">Kit press</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <ul class="list-unstyled social-list">
+          <li class="d-inline-block"><a href="https://www.facebook.com/groups/opalfirststep/" target="_blank"><img src="img/facebook.svg" class="facebook text-hide" alt="Facebook"></a></li>
+          <li class="d-inline-block"><a href="https://www.instagram.com/festivalopal/" target="_blank"><img src="img/instagram.svg" class="instagram text-hide" alt="Instagram"></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="popin">
+  <a href="#" class="close text-hide">Fermer</a>
+  <h3 class="section-title">Envie de participer ?</h3>
+  <p class="text-center">Tu as acheté ton billet mais tu veux aussi aider à la création et la vie du festival, dis nous ce dont tu as envie et on va t’aider à rejoindre l’équipe</p>
+  <div class="text-center">
+    <div><a href="https://opal-festival.typeform.com/to/BQYRRd" target="_blank" class="btn btn-chevron btn-outline">Participer en tant qu'artiste</a></div>
+    <div><a href="https://docs.google.com/forms/d/e/1FAIpQLSfMbvu5Ks1MZg5mJJ7R0WISFwIZIvg7ddrYeuSpBj3e59AyrA/viewform" target="_blank" class="btn btn-chevron btn-outline">Aider en cuisine</a></div>
+    <div><a href="https://opal-festival.typeform.com/to/bJMvk0?fbclid=IwAR0fsTeiOzJN6jca_qvsFXaUiZG3ezAqiLqWdxvLZEzq7aglcT-RDK-Otbg" class="btn btn-chevron btn-outline" target="_blank">Proposer une animation</a></div>
+<!--     <div><a href="https://www.facebook.com/groups/opalfirststep/" target="_blank" class="btn btn-chevron btn-outline">Aider à la construction</a></div> -->
+  </div>
+</div>
+
+<!-- script.js:js -->
+<script src="js/jquery/jquery-3.3.1.js"></script>
+<script src="js/libs/imagesloaded.pkgd.min.js"></script>
+<script src="js/libs/isotope.pkgd.js"></script>
+<script src="js/libs/jquery-form-elements.js"></script>
+<script src="js/libs/jquery-mary-popin.js"></script>
+<script src="js/script.js"></script>
+<!-- endinject -->
+
+<script>
+  window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+  ga('create', 'UA-91815596-1', 'auto');
+  ga('send', 'pageview');
+</script>
+<script async src='https://www.google-analytics.com/analytics.js'></script>
---
+++ dist/index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Opal 2019 - Créé le Festival de tes Rêves</title>
+    <meta name="description" content="Créer ensemble un évènement culturel et artistique. Opal est un évènement, un mouvement, un moment, une construction collective où chacun est libre de s’exprimer, de créer, d’enseigner, d’apprendre, d’expérimenter et d’exercer." />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="theme-color" content="#0d173a">
+    
+	<!-- Open Graph data -->
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Opal 2019 - Créé le Festival de tes Rêves" />
+	<meta property="og:description" content="Créer ensemble un évènement culturel et artistique. Opal est un évènement, un mouvement, un moment, une construction collective où chacun est libre de s’exprimer, de créer, d’enseigner, d’apprendre, d’expérimenter et d’exercer." />
+	<meta property="og:url" content="http://opal-festival.com/" />
+	<meta property="og:image" content="http://opal-festival.com/img/facebook-share.png" />
+	<meta property="og:site_name" content="Opal Festival" />    
+    
+    <!-- http://humanstxt.org -->
+    <link type="text/plain" rel="author" href="humans.txt" />
+    <link rel="stylesheet" href="style.css?v=1.4">
+    <link rel="dns-prefetch" href="//www.google-analytics.com">
+
+    <script type="application/ld+json">
+      "@context": "http://schema.org",
+      "@type": "Festival",
+      "name": "Opal Festival",
+      "alternateName": "Opal",
+      "description": "Créer ensemble un évènement culturel et artistique. Opal est un évènement, un mouvement, un moment, une construction collective où chacun est libre de s’exprimer, de créer, d’enseigner, d’apprendre, d’expérimenter et d’exercer.",
+      "startDate" : "2019-08-01",
+      "endDate" : "2019-08-05",
+      "location" : "La Goulafrière",
+      "url": "http://opal-festival.com",
+      "sameAs": [
+        "https://www.facebook.com/groups/opalfirststep/"
+      ],
+      "email" : "welcome@opal.events"
+    </script>
+  </head>
+
+  <body>
+<!------------------------------------------------------------------------
+	
+  _  ___  _  _              _  ___  _  _     _   _  _  __  _  _ _ //_   ||
+ / \| o \/ \| |            / \| o \/ \| |   | \_/ |/ \/ _|/ \| | | __|  ||
+( o )  _/ o | |_          ( o )  _/ o | |_  | \_/ | o \_ ( o ) U | _|    
+ \_/|_| |_n_|___() () ()   \_/|_| |_n_|___| |_| |_|_n_|__/\_,7|___|___| ()
+
+-------------------------------------------------------------------------->
+	  
+    <?php // require('header.php'); ?>
+
+    <div class="header">
+      <img src="img/mobile-bg@3x.png" class="d-block d-md-none img-fluid" alt="Opal - 1 au 5 août 2019">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-5 col-lg-6 order-md-2 d-none d-md-block">
+            <div class="owl">
+              <img src="img/owl.png" srcset="img/owl@2x.png 2x" class="owl-visual" alt="">
+            </div>
+          </div>
+          <div class=" col-md-7 col-lg-6 order-md-1">
+            <h1 class="main-logo">
+              <img src="img/logo-couleur.png" class="d-none d-md-block" width="220" alt="Opal - 1 au 5 août 2019">
+            </h1>
+            <h2 class="baseline">Rejoins la communauté Opal</h2>
+            <p class="text-center">et construis le festival de tes rêves</p>
+            <div class="text-center">
+              <div><a href="#" target="_blank" class="btn btn-chevron">Prendre mon billet (bientôt)</a></div>
+              <div><a href="#" class="btn btn-chevron btn-outline popin-trigger">J'ai envie de participer</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="home-block about">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-10">
+            <h3 class="section-title">C'est quoi Opal ?</h3>
+            <p class="text-center">Opal est une communauté autogérée qui crée chaque année un festival participatif en Normandie.</p>
+
+            <div class="row justify-content-center">
+              <div class="col-lg-10 col-xl-8">
+                <div class="video-wrapper">
+                 	<iframe width="640" height="360" src="https://www.youtube.com/embed/LAd98fX81G4?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+              </div>
+            </div>		        
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="frieze">
+      <img src="img/pola.png" srcset="img/pola@2x.png 2x" class="frieze-visual img-fluid" alt="Photos des éditions 2017-2018 du festival Opal">
+    </div>
+
+    <div class="home-block chat">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-10">
+            <ul class="chat-bubbles list-unstyled">
+              <li class="bubble-left">
+                <span class="avatar">
+                  <img src="img/avatar-owl.png" srcset="img/avatar-owl@2x.png 2x" alt="Opal">
+                </span>
+                <span class="msg">Yo ! Tu viens à Opal avec nous pour créer le festival 2019 ?</span>
+              </li>
+              <li class="bubble-right">
+                <span class="avatar">
+                  <img src="img/avatar-cat1.png" srcset="img/avatar-cat1@2x.png 2x" alt="Moi">
+                </span>
+                <span class="msg">Opal ?</span>
+              </li>
+              <li class="bubble-left">
+                <span class="avatar">
+                  <img src="img/avatar-owl.png" srcset="img/avatar-owl@2x.png 2x" alt="Opal">
+                </span>
+                <span class="msg">C’est une communauté qui crée un festival participatif entièrement géré par les festivaliers. On sera 300 cette année. Y’a des concerts 🎵, des animations 🤹‍♂️, un bar avec 1000 litres de 🍻 à se partager, un kitchen camp 👨‍🍳… mais surtout des gens cools</span>
+              </li>
+              <li class="bubble-right">
+                <span class="avatar">
+                  <img src="img/avatar-cat2.png" srcset="img/avatar-cat2@2x.png 2x" alt="Moi">
+                </span>
+                <span class="msg">😍 Du coup comment je contribue ?</span>
+              </li>
+              <li class="bubble-left">
+                <span class="avatar">
+                  <img src="img/avatar-owl.png" srcset="img/avatar-owl@2x.png 2x" alt="Opal">
+                </span>
+                <span class="msg">On a besoin de toi pour construire la scène, réfléchir à la déco, optimiser le plan du terrain, définir le lineup, tester la bière, inventer des animations... </span>
+              </li>
+			  <li class="bubble-left">
+                <span class="avatar">
+                  <img src="img/avatar-owl.png" srcset="img/avatar-owl@2x.png 2x" alt="Opal">
+                </span>
+				<span class="msg">On y sera à partir du lundi 29 juillet pour commencer à fabriquer des trucs ⚒ et faire un peu la fête 👯‍♀️.</span>
+              </li>
+              <li class="bubble-right">
+                <span class="avatar">
+                  <img src="img/avatar-cat3.png" srcset="img/avatar-cat3@2x.png 2x" alt="Moi">
+                </span>
+                <span class="msg">Trop cool ! Et c’est combien le billet ?</span>
+              </li>
+              <li class="bubble-left">
+                <span class="avatar">
+                  <img src="img/avatar-owl.png" srcset="img/avatar-owl@2x.png 2x" alt="Opal">
+                </span>
+                <span class="msg">T’es libre de mettre ce que tu veux.</span>
+              </li>
+              <li class="bubble-left">
+                <span class="avatar">
+                  <img src="img/avatar-owl.png" srcset="img/avatar-owl@2x.png 2x" alt="Opal">
+                </span>
+                <span class="msg">En gros, ça revient à 40€ par personne : construction de la scène, location sono, déco, 🍻... ! Mais c’est toi qui décide.<br> 
+T’auras rien à acheter sur place (pas d’argent sur le festival).
+</span>
+              </li>
+              <li class="bubble-right">
+                <span class="avatar">
+                  <img src="img/avatar-cat4.png" srcset="img/avatar-cat4@2x.png 2x" alt="Moi">
+                </span>
+                <span class="msg">Tu me fais rêver !</span>
+              </li>
+              <li class="bubble-left">
+                <span class="avatar">
+                  <img src="img/avatar-owl.png" srcset="img/avatar-owl@2x.png 2x" alt="Opal">
+                </span>
+                <span class="msg">Fais pas ton petit 🐈 et retrouve toutes les infos sur le groupe Facebook !</span>
+              </li>
+              <li class="bubble-left">
+                <span class="avatar">
+                  <img src="img/avatar-owl.png" srcset="img/avatar-owl@2x.png 2x" alt="Opal">
+                </span>
+                <span class="msg">❤️</span>
+              </li>
+            </ul>
+
+            <div class="text-center">
+              <a href="https://www.facebook.com/groups/opalfirststep/" target="_blank" class="btn btn-chevron">Plus d'infos sur facebook</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="home-block values">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-10">
+            <h3 class="section-title">OK mais c’est quoi un « festival participatif » ?</h3>
+            <p class="text-center">A Opal, il n’y a pas d’orga, de bénévoles, d’artistes ou de festivaliers, il n’y a que des participants. Opal, c’est le festival que Tu construis. </p> 
+            <p class="text-center">Pas de hiérarchie, pas de règles, le seul guide, ce sont les valeurs d’Opal.</p>
+            
+
+            <ul class="list-unstyled values-list">
+              <li>Transmission</li>
+              <li>Curiosité</li>
+              <li>Responsabilité</li>
+              <li>Confiance</li>
+              <li>Liberté</li>
+              <li>Tolérance</li>
+              <li>Bienveillance</li>
+<!--          <li>Forey (pour les vrais)</li> -->
+            </ul>
+
+            <div class="text-center">
+              <a href="https://trello.com/c/QhFeEUfq/29-vision" class="btn btn-chevron" target="_blank">Découvre la vision du festival</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="home-block lastyear">
+      <div class="wave">
+        <svg width="2560px" height="945px" viewBox="0 0 2560 945" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="1-HOMEPAGE" transform="translate(0.000000, -1614.000000)" fill="#0E1E4D">
+                    <path d="M0,1630.07323 L2.2382836,2372.09119 C520.610453,2177.03686 978.955158,2177.03686 1377.2724,2372.09119 C1775.58964,2567.14553 2169.83217,2609.53373 2560,2499.2558 L2560,1800.10862 C2221.34779,1925.84511 2005.4015,2005.77626 1912.16112,2039.90207 C1644.04802,2138.03099 1288.15144,1990.20095 999.784707,1846.30524 C607.788188,1650.698 274.526619,1578.62067 0,1630.07323 Z" id="VAGUE"></path>
+                </g>
+            </g>
+        </svg>
+      </div>
+    </div>
+
+    <?php require('footer.php'); ?>
+    <?php require('ascii.php'); ?>    
+    
+  </body>
+</html>
